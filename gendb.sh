29991cf1756ba53f24c5b7e8eaa6d3cb918c7e96
zhaose <70591705+weiliang1503@users.noreply.github.com>
2021-12-20 22:33:16 +0800

    Update gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir aya -p
+mkdir tmp -p
 
-urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/$1/releases/tag/x86_64 -s|grep "/$1/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 pkg=$(echo $i|awk -F '/' '{print$7}')
 echo Downloading $pkg
-wget -q https://github.com/$i -P aya/
-repo-add -p aya.db.tar.gz ./aya/$pkg
+wget -q https://github.com/$i -P tmp/
+repo-add -p ${2}.db.tar.gz ./tmp/$pkg
 done
 
