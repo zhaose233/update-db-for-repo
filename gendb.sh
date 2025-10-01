386a6afcbeb8fe766564521ea5b0f5d966db2614
Zhaose <70591705+zhaose233@users.noreply.github.com>
2025-10-02 02:26:23 +0800

    Update gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -9,9 +9,7 @@ API_URL="https://api.github.com/repos/$1/releases/tags/x86_64"
 urls=$(curl -sL "${API_URL}" | jq -r '.assets[].browser_download_url')
 
 for i in ${urls[*]}; do 
-pkg=$(echo $i|awk -F '/' '{print$7}')
-echo Downloading $pkg
-wget -q https://github.com/$i -P tmp/
+wget -q "$i" -P tmp/
 repo-add -p ${2}.db.tar.gz ./tmp/*.pkg.tar.zst
 done
 
